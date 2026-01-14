8c34ce18897a5077e618ff2134644b31d51b1b44
ShubhamSKA <skumarag@purdue.edu>
2026-01-13 20:12:56 -0500

    just passed over the display files, need to actually fix this thing

--- lib/display.c
+++ lib/display.c
@@ -4,60 +4,12 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-#include <stdio.h>
-#include <math.h>
-
-#include "pico/stdlib.h"
-#include "hardware/pio.h"
-#include "hardware/gpio.h"
-#include "hardware/interp.h"
+#include "display.h"
 
 #include "font.h"
 
 #include "main.pio.h"
 // #include "raspberry_256x256_rgb565.h"
-
-// Tested with the parts that have the height of 240 and 320
-#define SCREEN_WIDTH 240
-#define SCREEN_HEIGHT 240
-#define IMAGE_SIZE 256
-#define LOG_IMAGE_SIZE 8
-
-#define PIN_DIN 0
-#define PIN_CLK 1
-#define PIN_CS 2
-#define PIN_DC 3
-#define PIN_RESET 4
-#define PIN_BL 5
-
-// Define some 16-bit RGB565 colors
-#define BLACK   0x0000
-#define RED     0xF800
-#define GREEN   0x07E0
-#define BLUE    0x001F
-#define WHITE   0xFFFF
-#define YELLOW  0xFFE0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-
-// Define some 24-bit RGB888 colors
-#define GRAY   0x888888
-#define LIGHT_GRAY   0x444444
-#define LIGHT_GRAY   0x444444
-
-// Define ST7789 commands
-#define ST7789_CMD_CASET 0x2A
-#define ST7789_CMD_RASET 0x2B
-#define ST7789_CMD_RAMWR 0x2C
-
-#define SERIAL_CLK_DIV 1.f
-
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
-
-uint16_t framebuffer[SCREEN_WIDTH * SCREEN_HEIGHT] = {0};  // Each element is one pixel (RGB565)
-
 
 // Format: cmd length (including cmd byte), post delay in units of 5 ms, then cmd payload
 // Note the delays have been shortened a little
@@ -350,12 +302,9 @@ int main() {
     // lcd_draw_rect(pio, sm, 0, 0, 60, 60, rgbto565(GRAY));
     lcd_draw_circle(120,120, 16, GREEN);
     lcd_draw_circle_fill(120, 180, 33, rgbto565(0xFF3399));
-    lcd_draw_string(80, 80, "Shubham Was Here", BLUE);
-    lcd_draw_char(10, 10, 'B', CYAN);
+    // lcd_draw_string(80, 80, "Shubham Was Here", BLUE);
+    // lcd_draw_char(10, 10, 'B', CYAN);
     lcd_update(pio, sm);
     lcd_draw_progress_bar(pio, sm, 200, 46);
     
-    while (1) {
-        
-    }
 }
