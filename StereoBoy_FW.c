ec48d96f3174f02c085888c50b00f8bd71532a5a
ShubhamSKA <skumarag@purdue.edu>
2026-01-22 11:44:55 -0500

    display works

--- StereoBoy_FW.c
+++ StereoBoy_FW.c
@@ -405,27 +405,34 @@ int main() {
     gpio_set_dir(LED_PIN, GPIO_OUT);
     PIO pio = pio0;
     uint sm = 0;
-    gpio_init(PIN_CS);
+    gpio_init(PIN_CS_DISPLAY);
     gpio_init(PIN_DC);
     gpio_init(PIN_RESET);
     gpio_init(PIN_BL);
-    gpio_set_dir(PIN_CS, GPIO_OUT);
+    gpio_set_dir(PIN_CS_DISPLAY, GPIO_OUT);
     gpio_set_dir(PIN_DC, GPIO_OUT);
     gpio_set_dir(PIN_RESET, GPIO_OUT);
     gpio_set_dir(PIN_BL, GPIO_OUT);
     gpio_put(LED_PIN, 1);
-    gpio_put(PIN_CS, 1);
+    gpio_put(PIN_CS_DISPLAY, 1);
     gpio_put(PIN_RESET, 1);
     lcd_init(pio, sm, st7789_init_seq);
     gpio_put(PIN_BL, 1);
-    lcd_draw_circle(120,120, 16, GREEN);
-    lcd_draw_circle_fill(120, 180, 33, rgbto565(0xFF3399));
-    lcd_draw_string(80, 80, "Shubham Was Here", BLUE);
-    lcd_draw_char(10, 10, 'B', CYAN);
+    // lcd_draw_circle(120,120, 16, GREEN);
+    // lcd_draw_circle_fill(120, 180, 33, rgbto565(0xFF3399));
+    lcd_draw_string(0, 0, "Shubham Was Here", BLUE);
+    // lcd_draw_char(10, 10, 'B', CYAN);
     lcd_update(pio, sm);
-    lcd_draw_progress_bar(pio, sm, 200, 46);
+    // lcd_draw_progress_bar(pio, sm, 200, 46);
     
     ///////////////////////////DISPLAY END///////////////////////////
+
+    //////////////////////////LED DRIVER/////////////////////////////
+    
+
+
+
+    //////////////////////////LED END////////////////////////////////
     while (1) {
         // --- Print menu ---
         printf("\r\nAvailable tracks:\r\n");
